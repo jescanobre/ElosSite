99318b3bbf7eacfa3e535e7bed7a7135a87e711d
jescanobre <jescanobre@gmail.com>
2018-12-09 18:32:43 -0200

    up

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { routing } from './app.rounting';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -26,7 +27,8 @@ import { PersonagemDetalheComponent } from './personagem-detalhe/personagem-deta
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
--- project/src/app/home/home.component.ts
+++ project/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 // import { element, $ } from 'protractor';
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
+import { Router, RouterModule } from '../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-home',
