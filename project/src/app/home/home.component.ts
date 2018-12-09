4366e790904a747645ed531bdc25fc6c91a7bca7
jescanobre <jescanobre@gmail.com>
2018-12-09 16:50:31 -0200

    up

--- project/src/app/home/home.component.ts
+++ project/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
+// import { element, $ } from 'protractor';
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +12,34 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  
   }
+
+  public imgPersonagens:Array<string> = [ 
+    "../../assets/images/Armature_escudo_27.png",
+    "../../assets/images/Armature_atk_33.png"
+  ];
+
+  public imgPersonagensMap = new Map([
+    ["limonada",  "../../assets/images/Armature_escudo_27.png",],
+    ["zonzo",  "../../assets/images/Armature_atk_33.png"]
+  ]);
+
+
+  mudarImg(event){
+    event.preventDefault();
+    let el = event.currentTarget;
+    let id = event.currentTarget.id;
+
+    let img = this.imgPersonagensMap.get(id);
+
+    let element = $('#displayImg');
+    element.attr('src',img);
+
+    if(id == 'zonzo')
+      $('#displayName').html('ZONZO');
+    else if(id == 'limonada')
+      $('#displayName').html('LIMONADA');
+  };
 
 }
