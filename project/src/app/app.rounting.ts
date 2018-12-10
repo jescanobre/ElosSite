a4453572082eda293e0e1731a39733f10588ea3e
mariajesca <jescanobre@gmail.com>
2018-12-10 04:34:05 -0300

    conexao login e cadastro

--- project/src/app/app.rounting.ts
+++ project/src/app/app.rounting.ts
@@ -1,6 +1,8 @@
 import { HomeComponent } from './home/home.component';
 import { PersonagemDetalheComponent } from './personagem-detalhe/personagem-detalhe.component';
 import { StoreComponent } from './store/store.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 import { Routes, RouterModule } from '@angular/router';
 
@@ -8,9 +10,9 @@ const APP_ROUTES: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'perfilpersonagem', component: PersonagemDetalheComponent},
-    {path: 'loja', component: StoreComponent}
-    // {path: 'login', component: LoginComponent},
-    // {path: 'cadastro', component: CadastroComponent},
+    {path: 'loja', component: StoreComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'cadastro', component: CadastroComponent},
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
--- project/src/app/cadastro/cadastro.component.ts
+++ project/src/app/cadastro/cadastro.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgModel } from '@angular/forms';
-
+import { User } from '../models/user.model';
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,17 +12,44 @@ import { NgModel } from '@angular/forms';
 })
 export class CadastroComponent implements OnInit {
 
-  _id: string;
-  name: string;
-  email: string;
-  password: string;
+  private urlLogin = 'https://us-central1-game-test-8ed44.cloudfunctions.net/login'
+  private urlNickName = 'https://us-central1-game-test-8ed44.cloudfunctions.net/app/changeNickname?token=';
+  private urlCadastro = 'https://us-central1-game-test-8ed44.cloudfunctions.net/createAccount';
+  user = new User();
   confirmPassword: string;
 
-  constructor() { }
+  constructor(private route : Router) { }
 
   ngOnInit() {
   }
 
-  addUser (){}
+  cadastrar(user : User) {
+
+    let obj = {email: user.email, password: user.password};
+
+    $.post(this.urlCadastro, obj).done( data => {
+      console.log(data);
+      $.post(this.urlLogin, obj).done( data => {
+        console.log(data);
+        let token = data.token;
+        this.salvarNickName(user, token);
+        user._id = token;
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        this.route.navigate(['/']);
+      });
+    });
+  }
+
+  salvarNickName(user : User, token : string) {
+    $.post(this.urlNickName + token, {nickname : user.nickname}).done( data => {
+      console.log(data);
+    });
+  }
+
+  addUser (event : Event){
+    event.preventDefault();
+
+    this.cadastrar(this.user);
+  }
 
 }
